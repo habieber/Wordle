03781db32a101cd81e26fe52229e54e14ec28f80
Holly Bieber <habieber1108@gmail.com>
2024-02-23 16:28:59 -0600

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,58 @@
+/*----- constants -----*/
+const WORDS_LIST = ['CODER', 'LOSER'];
+const MAX_GUESSES = 6;
+
+/*----- state variables -----*/
+
+let board;
+let guesses;
+let winner;
+let lose;
+
+let guessedWords = [[]];
+let nextSpace = 1;
+
+/*----- cached elements  -----*/
+const deleteBtn = document.getElementById('delete-key')
+const enterBtn = document.getElementById('enter-key')
+
+/*----- event listeners -----*/
+const letters = document.querySelectorAll('button');
+
+/*----- functions -----*/
+
+init();
+
+function init() {
+    renderBoard();
+}
+
+function renderBoard() {
+    const letters = document.querySelectorAll('button');
+
+    for (let i = 0; i < letters.length; i++) {
+        letters[i].onclick = ({target}) => {
+            const key = target.textContent;
+            
+            addLetter(key);
+        }
+    }
+}
+
+function getCurrentWordArr() {
+    const numberOfGuessedWords = guessedWords.length
+    return guessedWords[numberOfGuessedWords - 1]
+}
+
+function addLetter(letter) {
+    const CurrentWordArr = getCurrentWordArr()
+
+    if (CurrentWordArr && CurrentWordArr.length < 5) {
+        CurrentWordArr.push(letter)
+
+        const nextSpaceEl = document.getElementById(nextSpace)
+        nextSpace = nextSpace + 1
+
+        nextSpaceEl.textContent = letter;
+    }
+}
