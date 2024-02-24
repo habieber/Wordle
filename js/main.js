11faede81542822675f9a8f178d0578db44a9c78
Holly Bieber <habieber1108@gmail.com>
2024-02-24 11:49:10 -0600

    added word submission & win check

--- js/main.js
+++ js/main.js
@@ -4,8 +4,9 @@ const MAX_GUESSES = 6;
 
 /*----- state variables -----*/
 
-let board;
-let guesses;
+let board = [];
+let secretWord = WORDS_LIST[0];
+
 let winner;
 let lose;
 
@@ -13,47 +14,84 @@ let guessedWords = [[]];
 let nextSpace = 1;
 
 /*----- cached elements  -----*/
-const deleteBtn = document.getElementById('delete-key')
-const enterBtn = document.getElementById('enter-key')
+// const deleteBtn = document.getElementById('delete-key')
+// const enterBtn = document.getElementById('enter-key')
+// ^^ cached these, but then realized I didn't need to specifically access them
 
 /*----- event listeners -----*/
-const letters = document.querySelectorAll('button');
+const letters = document.querySelectorAll('button.key');
 
 /*----- functions -----*/
 
 init();
 
+
 function init() {
-    renderBoard();
+    board = document.querySelectorAll('board > div')
+    render();
 }
 
 function render() {
-    renderBoard();
-    renderMessage();
-    renderControls();
+    renderKeyboard();
+    // renderMessage();
+    // renderKeyboard();
 }
 
 function renderBoard() {
-    const letters = document.querySelectorAll('button.key');
+    // board.forEach() {
+
+    // }
+} 
+
+// makes the clicking of keys populate letters to the board
+function renderKeyboard() {
+
 //borrowed this for loop from youtube tutorial
     for (let i = 0; i < letters.length; i++) {
         letters[i].onclick = ({target}) => {
                 const key = target.textContent;
+                
+// diversion if enter or delete are clicked
+                if(key === 'ENTER') {
+                    handleSubmitGuess();
+                    return;
+                } else if(key === 'DEL') {
+                    handleDelete();
+                    return;
+                }
+
                 addLetter(key);
             }
         }
     }
 
+function handleSubmitGuess() {
+    console.log('submit guess function!')
+    const currentWordArr = getCurrentWordArr().join('');
+    if(currentWordArr.length !== 5){
+        return;
+    } else if(currentWordArr === secretWord) {
+        console.log('you win!');
+    }
+
+    guessedWords.push([]);
+}
+
+function handleDelete() {
+    console.log('delete function!')
+}
+
 function getCurrentWordArr() {
     const numberOfGuessedWords = guessedWords.length
     return guessedWords[numberOfGuessedWords - 1]
+    
 }
 
 function addLetter(letter) {
-    const CurrentWordArr = getCurrentWordArr()
+    const currentWordArr = getCurrentWordArr()
 
-    if (CurrentWordArr && CurrentWordArr.length < 5) {
-        CurrentWordArr.push(letter)
+    if (currentWordArr && currentWordArr.length < 5) {
+        currentWordArr.push(letter)
 
         const nextSpaceEl = document.getElementById(nextSpace)
         nextSpace = nextSpace + 1
